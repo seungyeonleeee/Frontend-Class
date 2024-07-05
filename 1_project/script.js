80811b783fc5b4fae439e8847f75960696f3cc14
seungyeonleeee <syyy0.02@gmail.com>
2024-07-05 18:59:49 +0900

    240705

--- 1_project/script.js
+++ 1_project/script.js
@@ -10,31 +10,102 @@ window.addEventListener("scroll", () => {
 
 //project_slide
 //button
-const slideBtns = document.querySelector(".project_controls");
-const prevBtn = slideBtns.querySelector(".prev");
-const nextBtn = slideBtns.querySelector(".next");
+const projectSlideBtns = document.querySelector(".project_controls");
+const projectPrevBtn = projectSlideBtns.querySelector(".prev");
+const projectNextBtn = projectSlideBtns.querySelector(".next");
 //slide
-const slideWrapper = document.querySelector(".project_slide_wrapper");
-const slide = slideWrapper.querySelectorAll("li");
+const projectSlideWrap = document.querySelector(".project_slide_wrap");
+const projectSlide = projectSlideWrap.querySelectorAll("li");
 //count
-const slideCount = slide.length;
-const slideWidth = 370;
-const slideMargin = 40;
+const projectSlideCount = projectSlide.length;
+const projectSlideWidth = 370;
+const projectSlideMargin = 40;
 //index setting
 let currentIdx = 0;
 
-const updateWidth = () => {};
+const updateWidth = () => {
+  const currentSlides = document.querySelectorAll(".project_slide_wrap li ");
+  const newSlideCount = currentSlides.length;
+  const newWidth = `${
+    (projectSlideWidth + projectSlideMargin) * newSlideCount -
+    projectSlideMargin
+  }px`;
+  projectSlideWrap.style.width = newWidth;
+};
+
+const setInitialPos = () => {
+  const initialTranslateValue =
+    -(projectSlideWidth + projectSlideMargin) * projectSlideCount;
+  projectSlideWrap.style.transform = `translateX(${initialTranslateValue}px)`;
+};
 
 const makeClone = () => {
-  for (let i = 0; i < slideCount; i++) {
-    const cloneSlide = slide[i].cloneNode(true);
+  for (let i = 0; i < projectSlideCount; i++) {
+    const cloneSlide = projectSlide[i].cloneNode(true);
     cloneSlide.classList.add("clone");
-    slideWrapper.appendChild(cloneSlide);
+    projectSlideWrap.appendChild(cloneSlide);
   }
-  for (let i = slideCount - 1; i >= 0; i--) {
-    const cloneSlide = slide[i].cloneNode(true);
+  for (let i = projectSlideCount - 1; i >= 0; i--) {
+    const cloneSlide = projectSlide[i].cloneNode(true);
     cloneSlide.classList.add("clone");
-    slideWrapper.prepend(cloneSlide);
+    projectSlideWrap.prepend(cloneSlide);
   }
+  updateWidth();
+  setInitialPos();
+  setTimeout(() => {
+    projectSlideWrap.classList.add("animated");
+  }, 100);
 };
 makeClone();
+
+const moveSlide = (num) => {
+  projectSlideWrap.style.left = `${
+    -num * (projectSlideWidth + projectSlideMargin)
+  }px`;
+  currentIdx = num;
+  if (currentIdx === projectSlideCount || currentIdx === -projectSlideCount) {
+    setTimeout(() => {
+      projectSlideWrap.classList.remove("animated");
+      projectSlideWrap.style.left = "0px";
+      currentIdx = 0;
+    }, 500);
+    setTimeout(() => {
+      projectSlideWrap.classList.add("animated");
+    }, 600);
+  }
+};
+
+projectNextBtn.addEventListener("click", () => {
+  moveSlide(currentIdx + 1);
+});
+projectPrevBtn.addEventListener("click", () => {
+  moveSlide(currentIdx - 1);
+});
+
+let timer = undefined;
+const autoProjectSlide = () => {
+  if (timer === undefined) {
+    timer = setInterval(() => {
+      moveSlide(currentIdx + 1);
+    }, 3000);
+  }
+};
+autoProjectSlide();
+
+const stopProjectSlide = () => {
+  clearInterval(timer);
+  timer = undefined;
+};
+
+projectSlideWrap.addEventListener("mouseenter", () => {
+  stopProjectSlide();
+});
+projectSlideWrap.addEventListener("mouseleave", () => {
+  autoProjectSlide();
+});
+projectSlideBtns.addEventListener("mouseenter", () => {
+  stopProjectSlide();
+});
+projectSlideBtns.addEventListener("mouseleave", () => {
+  autoProjectSlide();
+});
