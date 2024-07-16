0805af88c53a3787a1b245b444cb520f1c9a78d8
seungyeonleeee <syyy0.02@gmail.com>
2024-07-16 18:17:20 +0900

    240716

--- 240715/02flex/script.js
+++ 240715/02flex/script.js
@@ -1,4 +1,4 @@
-//header
+//header - nav
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   //console.log(scroll);
@@ -9,4 +9,42 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("active");
   }
+});
+
+//testimonial - slick slider
+$(".myslider").slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  reponsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
 });
