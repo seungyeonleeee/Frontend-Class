14354a8e728a05247e998abf14933fc007a1eeb5
seungyeonleeee <syyy0.02@gmail.com>
2024-07-17 18:17:26 +0900

    240717

--- 240715/02flex/script.js
+++ 240715/02flex/script.js
@@ -1,14 +1,33 @@
-//header - nav
+//header - nav, gototop
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   //console.log(scroll);
-
   const header = document.querySelector("header");
+  const gototop = document.querySelector(".gototop");
+  //console.log(gototop);
   if (scroll > 50) {
     header.classList.add("active");
+    gototop.classList.add("active");
   } else {
     header.classList.remove("active");
+    gototop.classList.remove("active");
   }
+});
+
+//trigger
+const trigger = document.querySelector(".trigger");
+const gnb = document.querySelector(".gnb");
+const gnbLinks = gnb.querySelectorAll("a");
+//console.log(trigger, gnb, gnbLinks);
+trigger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  gnb.classList.toggle("active");
+});
+gnbLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    trigger.classList.remove("active");
+    gnb.classList.remove("active");
+  });
 });
 
 //testimonial - slick slider
@@ -31,7 +50,7 @@ $(".myslider").slick({
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 900,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
