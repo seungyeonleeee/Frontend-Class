41a763a6aa3162251b5d35e996198513704f56ee
seungyeonleeee <syyy0.02@gmail.com>
2024-09-09 23:11:27 +0900

    240909

--- 240905/todolistreact/src/components/TestComp.js
+++ 240905/todolistreact/src/components/TestComp.js
@@ -1,4 +1,3 @@
-import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
 // 41
 // import { useState } from "react";
--- custom_project/hyundaiec/script.js
+++ custom_project/hyundaiec/script.js
@@ -1,30 +1,51 @@
 // about - countEvent
-// const sections = document.querySelectorAll(".cd-section");
+const aboutSection = document.querySelector("#about");
+
+console.log(aboutSection);
+
+if (aboutSection.classList.contains("visible")) {
+  console.log("visible");
+
+  const counterElements = document.querySelectorAll(".count");
+  const animateCounter = (element, start, end) => {
+    let startTime;
+    const duration = 1000;
+
+    const updateCounter = (currentTime) => {
+      if (!startTime) startTime = currentTime;
+
+      const progress = Math.min((currentTime - startTime) / duration, 1);
+
+      const value = Math.floor(progress * (end - start) + start);
+
+      element.innerText = value;
+
+      if (progress < 1) requestAnimationFrame(updateCounter);
+    };
+
+    requestAnimationFrame(updateCounter);
+  };
+  animateCounter(counterElements[0], 0, 775);
+  animateCounter(counterElements[1], 0, 62);
+  animateCounter(counterElements[2], 0, 885);
+  animateCounter(counterElements[3], 0, 14);
+}
 
 // sections.forEach((section) => {
 //   if (section.id === "about") {
-//     console.log(section);
-
 //     // const counterElements = document.querySelectorAll(".count");
-
 //     // const animateCounter = (element, start, end) => {
 //     //   let startTime;
 //     //   const duration = 1000;
-
 //     //   const updateCounter = (currentTime) => {
 //     //     if (!startTime) startTime = currentTime;
-
 //     //     const progress = Math.min((currentTime - startTime) / duration, 1);
-
 //     //     const value = Math.floor(progress * (end - start) + start);
 //     //     element.innerText = value;
-
 //     //     if (progress < 1) requestAnimationFrame(updateCounter);
 //     //   };
-
 //     //   requestAnimationFrame(updateCounter);
 //     // };
-
 //     // animateCounter(counterElements[0], 0, 775);
 //     // animateCounter(counterElements[1], 0, 62);
 //     // animateCounter(counterElements[2], 0, 885);
